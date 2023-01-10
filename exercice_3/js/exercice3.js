dc2ca1c6b596783d149eac8c0d3768b66b5e4864
lagou <nzolagoup.nguessan@aiesec.net>
2023-01-10 16:20:22 +0000

    update

--- exercice_3/js/exercice3.js
+++ exercice_3/js/exercice3.js
@@ -1,19 +1,31 @@
-/*
-Dans le <div>, modifiez la valeur de l'attribut id de navBar à socialNetworkNavigation , 
-à l'aide de la setAttributeméthode .
-*/
-
-let editNavBar = document.querySelector(" #navBar ")
-
-editNavBar.setAttribute ("id", "socialNetworkNavigation")
-
-/*
-Nous allons ajouter un nouveau <li>au <ul>
-Tout d'abord, créez une nouvelle <li>balise (utilisez la createElementméthode).
-Créez un nouveau nœud de texte avec "Déconnexion" comme texte spécifié.
-Ajoutez le nœud de texte au nœud de liste nouvellement créé ( <li>).
-Enfin, ajoutez ce nœud de liste mis à jour à la liste non ordonnée ( <ul>), 
-en utilisant la appendChildméthode.
 
 
-*/
+
+//1 Add the code above, to your HTML file 
+
+/*  2 In the <div>, change the value of the 
+id attribute from navBar to socialNetworkNavigation, using the setAttribute method. */
+
+changeid=document.getElementById("navBar");
+
+changeid.setAttribute("id","socialNetworkNavigation");
+
+//3 We are going to add a new <li> to the <ul>
+
+//3-1 First, create a new <li> tag (use the createElement method).
+
+let newLi = document.createElement('li') 
+
+//3-2 Create a new text node with “Logout” as its specified text.
+
+let newTextNode = document.createTextNode('Logout')
+
+//3-3 Append the text node to the newly created list node (<li>).
+newLi.appendChild(newTextNode)
+
+/* 3-4 Finally, append this updated list node to the unordered list 
+(<ul>), using the appendChild method.*/
+
+document.getElementsByTagName("ul")[0].appendChild(newLi)
+
+
--- exercice_4/js/exercice.js
+++ exercice_4/js/exercice.js
@@ -1,3 +1,75 @@
+
+/* 3 Initiate the array with 2 books of your choice (ie. Add manually 2 
+    books objects in the array */
+
+    allBooks = [
+    {
+        "title": "Etrange",
+        "author": "AlbertCamus",
+        "image ": "https://img.over-blog-kiwi.com/2/02/71/53/20160413/ob_bdd7c6_avt-albert-camus-1969.jpeg",
+        "alreadyRead": true
+    },
+    {
+        "title": "climbié",
+        "author": "bernardBinlinDadié",
+        "image ": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUp4ZOyJ3t59qACPTyJ6hCtqmFGAcn9qw1Xg&usqp=CAU",
+        "alreadyRead": false
+    }
+
+
+]
+
+// 4 Requirements : 
+
+/*4-1 Using the DOM, render the books inside an HTML
+ table (the HTML table must be added to the <div> created in part 1). */
+
+ //We create a element table
+ let newTable = document.createElement('table') 
+ // we append our great div with the table ceated
+ document.querySelector(".listBooks").appendChild(newTable)
+
+ //we great the first tr
+ let tr=document.createElement('tr')
+ // we use a loop for create each th 
+for (let index = 0; index < 3; index++) {
+//we append the first tr with table
+        document.getElementsByTagName('table')[0].appendChild(tr)
+        for (let index1 = 0; index1 < 1; index1++) {
+            let th=document.createElement('th')
+            document.getElementsByTagName('tr')[0].appendChild(th)
+        }
+}
+//we generate the thead of table
+document.getElementsByTagName('th')[0].textContent="book’s title"
+document.getElementsByTagName('th')[1].textContent="book’s author"
+document.getElementsByTagName('th')[2].textContent="book’s image"
+newTable.setAttribute("border","1px;")
+// set a background color
+
+document.getElementsByTagName('tr')[0].setAttribute("style","background-color:blue;color:white;font-weight:bold;")
+//for each books we take information and create a td
+allBooks.forEach((element,index)=> {
+    let tr1=document.createElement('tr')
+    newTable.appendChild(tr1)
+    let td1=document.createElement('td')
+    tr1.appendChild(td1)
+    let td2=document.createElement('td')
+    tr1.appendChild(td2)
+    let td3=document.createElement('td')
+    tr1.appendChild(td3)
+    let img=document.createElement('img')
+    td3.appendChild(img)
+    td1.textContent=element.title
+    td2.textContent=element.author
+    img.setAttribute('src',element["image "]);
+    //set a dimension for image
+    img.setAttribute("style","width:100px;height:100px;")
+    //control if the book is read 
+    if(element.alreadyRead){
+        td1.setAttribute("style","color:red;")
+    }
+});
 /*
 Dans le fichier js, créez un tableau appelé allBooks. Il s'agit d'un tableau d'objets.
 Chaque objet est un livre qui possède 4 clés (soit 4 propriétés) :
@@ -6,7 +78,7 @@ auteur,
 image : une url,
 déjàRead : qui est un booléen (vrai ou faux).
 */
-
+/*
 let pocketBook1 = {
     title: "avantureAmbigure",
     author: "cheikhHamidouKane",
@@ -36,8 +108,6 @@ let pocketBook4 = {
     valreadyRead: "yes"
 }
 
-/*
-Initiez le tableau avec 2 livres de votre choix 
-(c'est-à-dire ajoutez manuellement 2 objets livres dans le tableau)
 */
+
 
